aa0861d8ba3316cb1ad32d47954eea4df931657a
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-05-08 21:24:49 -0400

    added logo for the website

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// importing home icon from font Awesome 
+import { faHome } from '@fortawesome/free-solid-svg-icons'; 
+
+import logo from '../images/inkblog1.png';
 
 function Navbar() {
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to="/" className="navbar-logo">
-            Ermiyas Gulti <i className='fab f-typo3'/>
-          </Link>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <h1>Welcome to Ermiyas Gulti's Personal Portfolio Website <FontAwesomeIcon icon={faHome} /></h1>
+            </div>
 
-     
+
+
+           {/* Logo Image */}
+           <div className='logo'style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <img src={logo} alt="Logo" />
+          </div>
+
+         
         </div>
       </nav>
     </>
