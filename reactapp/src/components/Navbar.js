5d0e3db3e4aecf836c4f13971026c5406dde8dd4
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-05-08 22:34:20 -0400

    added an integrate Material-UI icon and update navigation links in Navbar components

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -1,30 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-// importing home icon from font Awesome 
-import { faHome } from '@fortawesome/free-solid-svg-icons'; 
-
+import FormatListBulletedSharpIcon from '@mui/icons-material/FormatListBulletedSharp';
 import logo from '../images/inkblog1.png';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  }
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  }
 
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <h1>Welcome to Ermiyas Gulti's Personal Portfolio Website <FontAwesomeIcon icon={faHome} /></h1>
-            </div>
+            <h1>Welcome to Ermiyas Gulti's Personal Portfolio Website <FormatListBulletedSharpIcon /></h1>
 
-
-
-           {/* Logo Image */}
-           <div className='logo'style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <img src={logo} alt="Logo" />
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
           </div>
 
-         
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/Home' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/About' className='nav-links' onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/Projects' className='nav-links' onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/Contacts' className='nav-links' onClick={closeMobileMenu}>
+                Contacts
+              </Link>
+            </li>
+          </ul>
+
+          {/* Logo Image */}
+          <div className='logo' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <img src={logo} alt="Logo" />
+          </div>
         </div>
       </nav>
     </>
