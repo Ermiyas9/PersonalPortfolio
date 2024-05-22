bd3e58ef457bf8d9ac7e07292f36047a0448e8c7
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-05-21 21:17:07 -0400

    Added new basic vita project structure for the portfolio website

---
+++ ErmiyasPersonalPortofolio/src/main.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import "@fontsource/outfit";
+import "@fontsource/roboto";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
