e0f909eef40096e1d5c6466efdd54fdfeee368a0
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-09-26 11:57:03 -0400

    Added social media icons and changed few the website css file.

--- reactapp/src/components/MainPage.js
+++ reactapp/src/components/MainPage.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/inkblog1.png';
 import contentImage from '../images/contentImage.jpg';
+import hireMeIcon from '../images/hireMePic.png';
+import githubIcon from '../images/githubImage.png';
+import linkedInIcon from '../images/linkInImage.png';
+import instaImageIcon from '../images/instaImage.png';
+import outlookImageIcon from '../images/outlookImage.png';
+import emailIcon from '../images/emailImage.png';
+
 
 
 
@@ -16,6 +23,7 @@ function App() {
         <div className="logo">
           <img src={logo} alt="Logo" />
         </div>
+        
     
         {/* Navigation Links */}
         <div className="headerNavLink">
@@ -24,24 +32,75 @@ function App() {
           <Link to="/Exprience" className="nav-link">Work Exprience</Link>
         </div>
 
+      <div className='imageAndIntroPara'>
+
+      
+
       {/* Page Content */}
         <div className="contentImage">
           <img src={contentImage} alt="contentImage" />
         </div>
+
+        <div className="hireMeIcon">
+          <img src={hireMeIcon} alt="Hire Me Icon" />
+          <Link to="/hireMe" className="logo">Hire Me</Link>
+        </div>
+
     
         <div className="midParagraphTxt">
           <p>Hello, My name is Ermiyas Gulti.  Software Engineering</p>
-          <br /> {/* Correct usage of <br /> */}
           <p>student passionate about coding and technology.</p> 
           <p>I live in Kitchener and I am looking for Co-op Jobs</p>
         </div>
-        
+
+      </div>
+
+       <span class="pageScroller" arial hidden="true">
+          <span>
+            <span>S</span>
+            <span>C</span>
+            <span>R</span>
+            <span>O</span>
+            <span>L</span>
+            <span>L</span>
+            <span> </span>
+            <span>D</span>
+            <span>O</span>
+            <span>W</span>
+            <span>N</span>
+          </span>
+        </span>
+
           {/* footer Links */}
           <div className="footerNavLink">
           <Link to="/Projects" className="nav-link">Projects</Link>
           <Link to="/Hobbies" className="nav-link">Hobbies</Link>
           <Link to="/Doggo" className="nav-link">Meet my Doggo</Link>
         </div>
+
+
+        {/* Social Media Icons */}
+
+        <div className="githubLogo">
+          <img src={githubIcon} alt="githubLogo" className="socialMediaIcons" />
+        </div>
+
+        <div className="linkedInLogo">
+          <img src={linkedInIcon} alt="linkedInLogo" className="socialMediaIcons" />
+        </div>
+
+        <div className="instaIcon">
+          <img src={instaImageIcon} alt="instaLogo" className="socialMediaIcons" />
+        </div>
+
+        <div className="outlookIcon">
+          <img src={outlookImageIcon} alt="outlookLogo" className="socialMediaIcons" />
+        </div>
+
+        <div className="emailIcon">
+          <img src={emailIcon} alt="emailLogo" className="socialMediaIcons" />
+        </div>
+
 
     </div>
   );
