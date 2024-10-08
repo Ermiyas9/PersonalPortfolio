9d7a5da9f00088534956c555e0b2648a9c844b65
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-10-08 14:41:12 -0400

    For design purposes, I removed the menu bar and changed the useState to toggle between displaying the menu and closing it. In the 'menu' state, the website pages are displayed, and in the 'close' state, the menu is hidden, showing the home page instead

--- reactapp/src/components/Header.js
+++ reactapp/src/components/Header.js
@@ -1,41 +1,59 @@
 import { useState } from "react";
 import React from "react";
-
+import { Link } from 'react-router-dom';
 function HeaderComponents() {
 
-    // Initialize with 'Menu'
-  const [activeTab, setActiveTab] = useState("Menu"); 
+  // Initialize with 'Menu'
+  const [activeTabName, setActiveTabName] = useState("Menu");
 
-
-  const handleTabClick = (tabName) => {
-    setActiveTab(tabName);
+  const handleTabClick = (activeTabName) => {
+    setActiveTabName(activeTabName);
   };
 
   return (
 
-    <div id="headerTab" className="headerTab">
+    <div id="menuDropDown" className="dropDownMenu">
 
-      {/* Tab Buttons */}
+      {/* Tab Buttons doing it without showing the actual button for designing purpose  */}
       <div id="menuButton" className="menuBtn">
-        
-        <button
-          className={`tablink ${activeTab === "Menu" ? "active" : ""}`}
+        <div
+          className={`tablink ${activeTabName === "Menu" ? "active" : ""}`}
           onClick={() => handleTabClick("Menu")}
         >
           Menu
-        </button>
-        <button
-          className={`tablink ${activeTab === "HireMe" ? "active" : ""}`}
-          onClick={() => handleTabClick("HireMe")}
+        </div>
+        <div
+          className={`tablink ${activeTabName === "Close" ? "active" : ""}`}
+          onClick={() => handleTabClick("Close")}
         >
-          Hire Me
-        </button>
+          Close
+          <div className="headerNavLink">
+            <Link to="/" className="nav-link">Home</Link>
+          </div>
+          {/* i might can use this swith or the close button to close the menu and display home screen */}
+        </div>
       </div>
 
       {/* Tab Content */}
-      <div className="tabContent">
-        {activeTab === "Menu" && <h1>Menu</h1>}
-        {activeTab === "HireMe" && <h1>Hire Me</h1>}
+      <div className="menuBtnOptions">
+        {activeTabName === "Menu" && (
+          <>
+            <div className="menuBtnOptions">
+              {activeTabName === "Menu" && (
+                <div className="menuList">
+                  <ul>
+                    <li>HOME</li>
+                    <li>ABOUT ME</li>
+                    <li>MY PROJECTS</li>
+                    <li>MY DOGGO</li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+
+        {activeTabName === "Close" && <h1>Close</h1>}
       </div>
     </div>
   );
