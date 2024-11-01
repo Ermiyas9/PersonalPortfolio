63a1460f8b57b72eda1ff975e31afa39a73ff786
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2024-11-01 15:45:34 -0400

    Added dog image and updated messaging in the dog section.Also I have deployed the change into the server by using static server

--- reactapp/src/components/Doggo.js
+++ reactapp/src/components/Doggo.js
@@ -7,7 +7,9 @@ import instaImageIcon from '../images/dogoPage/instaIcon.png';
 import outlookImageIcon from '../images/dogoPage/outlookIcon.png';
 import emailIcon from '../images/dogoPage/gmailIcon.png';
 
-import dogPic from '../images/dogoPage/Aaron3.png';
+import dogPic from '../images/dogoPage/Aaron1.JPG';
+import dogMovie from '../images/dogoPage/aaronChill.mp4';
+
 import SocialMediaIconsDisplay from './socialMediaDisplay'; 
 
 function DoggoPage() {
@@ -26,18 +28,88 @@ function DoggoPage() {
                 gmailIcon={emailIcon} 
                 instaIcon={instaImageIcon}
             />
-
-            <div className="doggoContents">
+            
+            <div className="doggoContents" style={{ fontSize:'40px',fontWeight:'Bolder',fontFamily:'Courier New, monospace ',display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <p><h1>Hey There!</h1> </p>
 
                 <img 
                     src={dogPic} 
                     alt="Dog Picture"  
-                    style={{ width: '400px',height: '400px',borderRadius: '50%',marginRight: '25px',marginLeft: '5px',marginTop: '25px'
-
-                    }}
+                    style={{alignItems:'center', width: '500px', height: '600px', borderRadius: '50%', marginRight: '25px', marginLeft: '5px', marginTop: '25px' }}
                 />
             </div>
+
+            <div className="dogMsg" style={{
+                fontSize: '25px',
+                fontFamily: 'Courier New, monospace',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                    <p>Yo, what's up 🤗? My name is Aaron; I am a year-and-a-half dog.</p>
+                    <p>Still a puppy 🥰. I live with my best friend Ermiyas.</p>
+                    <p>I don't want to take your time by telling you how Ermi takes my personal space.</p>
+                    <p>If you don't hire him, I won't have a long nap.</p>
+
+                    <p><span>Please,  </span>
+                    <a
+                      href="mailto:egulti4128@conestogac.on.ca"
+                      style={{
+                        textDecoration: 'none',
+                        color: 'Blue',
+                        fontWeight: 'bold'
+                      }}
+                    >
+                      HIRE HIM
+                    </a>
+                    <span>, and Let me have loooooong nap all afternoon .</span>
+                  </p>
+                </div>
+            </div>
+
+
+    
+
+            
+            <div className="doggoContents" style={{
+                paddingBottom: '250px',
+                fontSize: '40px',
+                fontWeight: 'bolder',
+                fontFamily: 'Courier New, monospace',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                textAlign: 'center'
+            }}>
+                <p>
+                    <h3>Look at me, if he doesn’t have a job, he’ll be bothering me 😜😒</h3>
+                </p>
+
+                <iframe
+                    width="600"
+                    height="400"
+                    src={dogMovie}
+                    title="Dog video"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    style={{
+                        border: '5px solid #ccc',
+                        borderRadius: '15px',
+                        boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)',
+                        marginTop: '20px'
+                    }}
+                ></iframe>
+            </div>
+
+            <div>
+
+                <p className="doggoContents" >
+                    If you want to keep in touch with me, Follow my Instagram. And Forget what I said. I do love Ermiyas. 
+                </p>
+            </div>
+
+
 
            
 
