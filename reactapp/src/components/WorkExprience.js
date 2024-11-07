6d4125270df3b84cd5a7ced97d4dbaaec8d76e01
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2024-11-07 15:13:06 -0500

    Started working on my Exprience page by adding programming languages icons

--- reactapp/src/components/WorkExprience.js
+++ reactapp/src/components/WorkExprience.js
@@ -10,6 +10,16 @@ import emailIcon from '../images/workPage/gmailIcon.png';
 import instaImageIcon from '../images/workPage/instaIcon.png';
 import SocialMediaIconsDisplay from './socialMediaDisplay'; 
 
+// programming languages icons
+import reactIcon from '../images/projectsPage/reactIcon.png'; 
+import cSharpIcon from '../images/workPage/cSharpIcon.png'; 
+import cPlusPlusIcon from '../images/workPage/cPlusIcon.png'; 
+import pythonIcon from '../images/projectsPage/pythonIcon.png';
+import djangoIcon from '../images/projectsPage/djangoIcon.png'; 
+import cIcon from '../images/workPage/cIcon.png'; 
+import htmlIcon from '../images/projectsPage/htmlIcon.png'; 
+import androidIcon from '../images/projectsPage/androidIcon.png'; 
+
 function WorkExpriencePage() {
     return (
 
@@ -31,9 +41,39 @@ function WorkExpriencePage() {
             instaIcon={instaImageIcon}
         />
 
+        <div>
+            <p className='titles'> Technical Skills </p>
+            <p className='subTitles'>Programming Languages</p>
 
-             {/* calling footer component for the webste footer contents */}
-             <FooterComponents/>
+            <img 
+                src={cIcon} 
+                alt="Content"  
+                className="contentImage"
+                style={{ width: '50px', height: '50px' }}
+            />
+
+            <img 
+                src={cPlusPlusIcon} 
+                alt="Content"  
+                className="contentImage"
+                style={{ width: '50px', height: '50px' }}
+            />
+
+            <img 
+                src={cSharpIcon} 
+                alt="Content"  
+                className="contentImage"
+                style={{paddingTop: '15', paddingRight: '15',paddingLeft: '15',width: '50px', height: '50px' }}
+            />
+        </div>
+
+
+       
+
+
+
+        {/* calling footer component for the webste footer contents */}  
+        <FooterComponents/>
 
     </div>
     
