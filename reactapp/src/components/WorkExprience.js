438ab9e4bb3e0ec4cac1bc5a9b53627c6fd02862
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2024-11-12 16:08:47 -0500

    Added another btn on exprience page that will help employees easly to get a download access and view  of my achievement paper

--- reactapp/src/components/WorkExprience.js
+++ reactapp/src/components/WorkExprience.js
@@ -1,6 +1,5 @@
 // WorkExprience.js
-import React, { useState, useEffect } from 'react';
-
+import React, {useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import HeaderComponents from './Header';
 import FooterComponents from './Footer';
@@ -28,263 +27,240 @@ import blackDudeImage from '../images/workPage/blackDudeImage.png';
 import youngBlackMan from '../images/workPage/youngBlackMan.png'; 
 
 function WorkExpriencePage() {
-
-    useEffect(() => {
-        // Add event listener after component mounts
-        const downloadButton = document.getElementById("downloadButton");
-        if (downloadButton) {
-          downloadButton.addEventListener("click", function () {
-
-            // Path relative to the public folder
-            const pdfUrl = "/ErmiyasResume.pdf"; 
     
-            // Create an  element to trigger the download
-            const link = document.createElement("a");
-            link.href = pdfUrl;
+  useEffect(() => {
+    // Event handler to trigger download
+    const handleDownload = (pdfUrl, fileName) => {
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
 
-            // Custom download file name
-            link.download = "ErmiyasResume.pdf"; 
-    
-            // Append the link to the document body, click it, and remove it
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-          });
-        }
-    
-        // Cleanup the event listener when component unmounts
-        return () => {
-          if (downloadButton) {
-            downloadButton.removeEventListener("click", function () {});
-          }
-        };
-      }, []);
+    // Adding event listeners to both buttons
+    const downloadButton = document.getElementById("downloadButton");
+    const downloadButton1 = document.getElementById("downloadButton1");
 
-    return (
+    if (downloadButton) {
+      downloadButton.addEventListener("click", () => 
+        handleDownload("/ErmiyasResume.pdf", "ErmiyasResume.pdf")
+      );
+    }
+    if (downloadButton1) {
+      downloadButton1.addEventListener("click", () => 
+        handleDownload("/StudentAchievement.pdf", "StudentAchievement.pdf")
+      );
+    }
 
+    // Cleanup the event listeners when component unmounts
+    return () => {
+      if (downloadButton) {
+        downloadButton.removeEventListener("click", handleDownload);
+      }
+      if (downloadButton1) {
+        downloadButton1.removeEventListener("click", handleDownload);
+      }
+    };
+  }, []);
+
+  return (
     <div className="Exprience">
+      {/* passing the props to the header component(func) */}
+      <HeaderComponents hireMeIcon={hireMe} logo={logoIcon} />
 
-        {/* passing the props to the header component(func) */}
-        <HeaderComponents 
-            hireMeIcon={hireMe} 
-            logo={logoIcon} 
-        />
-           
-           
-       
+      {/* Pass the appropriate icons as props */}
+      <SocialMediaIconsDisplay
+        outlookIcon={outlookImageIcon}
+        gmailIcon={emailIcon}
+        instaIcon={instaImageIcon}
+      />
 
-        {/* Pass the appropriate icons as props */}
-        <SocialMediaIconsDisplay 
-            outlookIcon={outlookImageIcon} 
-            gmailIcon={emailIcon} 
-            instaIcon={instaImageIcon}
-        />
+      <img
+        src={blackDudeImage}
+        alt="Content"
+        className="contentImage"
+        style={{ paddingLeft: '550px', width: '400px', height: '400px' }}
+      />
 
-        <img 
-            src={blackDudeImage} 
-            alt="Content"  
-            className="contentImage"
-            style={{ paddingLeft: '550px',width: '400px', height: '400px' }}
-        />
+      <div>
+        <p className="titles"> 💻 Technical Skills </p>
+        <p className="subTitles">Programming Languages / Frameworks</p>
 
-
-        
-
-        <div>
-            <p className='titles'> 💻 Technical Skills </p>
-            <p className='subTitles'>Programming Languages / Frameworks</p>
-
-            
-            <img 
-                src={cIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '15', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                 src={cPlusPlusIcon}  
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '15', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={cSharpIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={assemblyIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-        </div>
-
-        
-        <div>
-            <img 
-                src={reactIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={pythonIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={androidIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={phpIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-      
-        </div>
-
-        
-        <div>
-
-            <img 
-                src={djangoIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '15', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={htmlIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={mySqlIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-
-            <img 
-                src={sqlIcon} 
-                alt="Content"  
-                className="contentImage"
-                style={{paddingTop: '5', paddingRight: '5',paddingLeft: '5',width: '50px', height: '50px' }}
-            />
-       
-        </div>   
-        
-        <p className='titles'> 📝 Education </p>
-        <p className='subTitles'>Advanced Diploma in Software Engineering Candidate</p>
-        <p style= {{paddingLeft: '150px'}}className='subTitles'> 🎓 Conestoga College, Waterloo | Sep. 2022 – Present | GPA 3.81</p>
-        <p className='subTitles'>Diploma in Information Communication Technology</p>
-        <h5><p style= {{paddingLeft: '150px'}}className='subTitles'>  🎓 Royal University College—Nazareth Ethiopia and Asella TVET Institute | Sep 2007 – July 2010 | GPA 3.85</p></h5>
-        
-
-        <p className='titles'> 🚀 Technical Experience </p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 Tech Representative, Conestoga College (Co-op) | Sep 2024 – Present</p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 Warehouse Worker, Grand River Personnel, Kitchener, ON | 2023</p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 Crew Member, McDonald’s, Kitchener, ON | 2020 – 2021</p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 Customer Service & Delivery Driver, Carriage & Talabat Delivery Company | 2016 - 2019</p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 Salesperson, Avenue Mall, Kuwait | 2014 - 2015</p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 IT Instructor, National College Addis Abeba, Ethiopia | 2012</p>
-        <p className='subTitles'style= {{paddingLeft: '150px'}}>🌟 Assistant IT Instructor, Rehoboth Medical College Asella, Ethiopia | 2010 – 2011</p>
-
-
-
-
-        <img 
-            src={youngBlackMan} 
-            alt="Content"  
-            className="contentImage"
-            style={{ paddingLeft: '550px',width: '500px', height: '400px' }}
+        <img
+          src={cIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '15', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
         />
 
+        <img
+          src={cPlusPlusIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '15', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
 
-<div
-  style={{
-    paddingTop: '75px',
-    paddingBottom: '75px',
-    paddingLeft: '250px',
-    fontFamily: "'Courier New', monospace",
-    fontWeight: 'bolder',
-    fontSize: '35px',
-  }}
->
-  <button
-    id="downloadButton"
+        <img
+          src={cSharpIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={assemblyIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+      </div>
+
+      <div>
+        <img
+          src={reactIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={pythonIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={androidIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={phpIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+      </div>
+
+      <div>
+        <img
+          src={djangoIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '15', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={htmlIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={mySqlIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+
+        <img
+          src={sqlIcon}
+          alt="Content"
+          className="contentImage"
+          style={{ paddingTop: '5', paddingRight: '5', paddingLeft: '5', width: '50px', height: '50px' }}
+        />
+      </div>
+
+      <p className="titles"> 📝 Education </p>
+      <p className="subTitles">Advanced Diploma in Software Engineering Candidate</p>
+      <p style={{ paddingLeft: '150px' }} className="subSecTitle"> 🎓 Conestoga College, Waterloo | Sep. 2022 – Present | GPA 3.81</p>
+      <p className="subTitles">Diploma in Information Communication Technology</p>
+      <p style={{ paddingLeft: '150px' }} className="subSecTitle"> 🎓 Royal University College—Nazareth Ethiopia and Asella TVET Institute | Sep 2007 – July 2010 | GPA 3.85</p>
+
+      <p className="titles"> 🚀 Technical Experience </p>
+      <p className="subTitles"> Career Highlights in Technical & Client-Focused Roles</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 Tech Representative, Conestoga College (Co-op) | Sep 2024 – Present</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 Warehouse Worker, Grand River Personnel, Kitchener, ON | 2023</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 Crew Member, McDonald’s, Kitchener, ON | 2020 – 2021</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 Customer Service & Delivery Driver, Carriage & Talabat Delivery Company | 2016 - 2019</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 Salesperson, Avenue Mall, Kuwait | 2014 - 2015</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 IT Instructor, National College Addis Abeba, Ethiopia | 2012</p>
+      <p className="subSecTitle" style={{ paddingLeft: '150px' }}>🌟 Assistant IT Instructor, Rehoboth Medical College Asella, Ethiopia | 2010 – 2011</p>
+
+      <img
+        src={youngBlackMan}
+        alt="Content"
+        className="contentImage"
+        style={{ paddingLeft: '550px', width: '500px', height: '400px' }}
+      />
+
+    <div
     style={{
-      display: 'block', 
-      fontSize: '35px',
-      color: 'blue',
-      fontWeight: 'bold',
-      fontFamily: "'Courier New', monospace",
-      boxShadow: '1px 5px 5px -3px green',
-      textDecoration: 'none',
-      padding: '10px 20px',
-      marginBottom: '20px', 
-      backgroundColor: '#C6B1F3',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
+        paddingTop: '75px',
+        paddingBottom: '75px',
+        paddingLeft: '250px',
+        fontFamily: "'Courier New', monospace",
+        fontWeight: 'bolder',
+        fontSize: '35px',
     }}
-  >
-   Resume
-  </button>
+    >
+    <button
+        id="downloadButton"
+        style={{
+        display: 'block', 
+        fontSize: '35px',
+        color: 'blue',
+        fontWeight: 'bold',
+        fontFamily: "'Courier New', monospace",
+        boxShadow: '1px 5px 5px -3px green',
+        textDecoration: 'none',
+        padding: '10px 20px',
+        marginBottom: '20px', 
+        backgroundColor: '#C6B1F3',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        }}
+    >
+    Resume
+    </button>
 
-  <button
-    id="downloadButton"
-    style={{
-      display: 'block', 
-      fontSize: '35px',
-      color: 'blue',
-      fontWeight: 'bold',
-      fontFamily: "'Courier New', monospace",
-      boxShadow: '1px 5px 5px -3px green',
-      textDecoration: 'none',
-      padding: '10px 20px',
-      marginBottom: '20px', 
-      backgroundColor: '#C6B1F3',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-    }}
-  >
-   Achievement Paper
-  </button>
+    <button
+        id="downloadButton2"
+        style={{
+        display: 'block', 
+        fontSize: '35px',
+        color: 'blue',
+        fontWeight: 'bold',
+        fontFamily: "'Courier New', monospace",
+        boxShadow: '1px 5px 5px -3px green',
+        textDecoration: 'none',
+        padding: '10px 20px',
+        marginBottom: '20px', 
+        backgroundColor: '#C6B1F3',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        }}
+    >
+    Achievement Paper
+    </button>
 
-  <p><Link to="/Projects" style={{ color:'blue',boxShadow:" 1px 5px 5px -3px green",textDecoration: 'none',}}>More Of My Projects </Link></p>
+    <p><Link to="/Projects" style={{ color:'blue',boxShadow:" 1px 5px 5px -3px green",textDecoration: 'none',}}>More Of My Projects </Link></p>
 
- 
-</div>
-
-
-
-
-
-
-
-        {/* calling footer component for the webste footer contents */}  
-        <FooterComponents/>
-
-    </div>
     
-    );
+    </div>
+
+      {/* passing the props to the header component(func) */}
+      <FooterComponents hireMeIcon={hireMe} logo={logoIcon} />
+    </div>
+  );
 }
 
 export default WorkExpriencePage;
