308a8589c9c7c979b1f501ac1a6f2a27bfc6896b
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2025-01-26 21:07:23 -0500

    Updates

---
+++ precache-manifest.51bc5aac1297a162ce02dda092854b80.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bde9420be71b4d48f8d91b7b7f17212",
+    "url": "/PersonalPortfolio/index.html"
+  },
+  {
+    "revision": "c5864016789dab7e59eb",
+    "url": "/PersonalPortfolio/static/css/2.4880e89e.chunk.css"
+  },
+  {
+    "revision": "922d8a7a89d7f5a287f2",
+    "url": "/PersonalPortfolio/static/css/main.02105932.chunk.css"
+  },
+  {
+    "revision": "c5864016789dab7e59eb",
+    "url": "/PersonalPortfolio/static/js/2.c23f9cb8.chunk.js"
+  },
+  {
+    "revision": "922d8a7a89d7f5a287f2",
+    "url": "/PersonalPortfolio/static/js/main.95ac4d24.chunk.js"
+  },
+  {
+    "revision": "c3cda49bd7d6e22d16ea",
+    "url": "/PersonalPortfolio/static/js/runtime~main.7462302f.js"
+  },
+  {
+    "revision": "357719d59e08926c1d4cc1870d6a4e26",
+    "url": "/PersonalPortfolio/static/media/Aaron1.357719d5.JPG"
+  },
+  {
+    "revision": "f7592399443c5ab31276499819563c7f",
+    "url": "/PersonalPortfolio/static/media/Aaron2.f7592399.JPG"
+  },
+  {
+    "revision": "57aa8c6e7a2c145ef3dbdd70559e21cf",
+    "url": "/PersonalPortfolio/static/media/Aaron3.57aa8c6e.JPG"
+  },
+  {
+    "revision": "4242b3eabdd460fd615cfed5662b7513",
+    "url": "/PersonalPortfolio/static/media/aaronChill.4242b3ea.mp4"
+  },
+  {
+    "revision": "dfc03a985b19cb20040c505e19932df0",
+    "url": "/PersonalPortfolio/static/media/bankAndTeller.dfc03a98.png"
+  },
+  {
+    "revision": "854e59c928fd6e3a0d2c612a411199d1",
+    "url": "/PersonalPortfolio/static/media/blackDudeImage.854e59c9.png"
+  },
+  {
+    "revision": "f99bde443362fe38cbe40f84fe4b688c",
+    "url": "/PersonalPortfolio/static/media/coding.f99bde44.png"
+  },
+  {
+    "revision": "30237146367ada6430fdd24a2fcf63f9",
+    "url": "/PersonalPortfolio/static/media/coffee.30237146.mp4"
+  },
+  {
+    "revision": "de3018fee060539136a1c070f982ab24",
+    "url": "/PersonalPortfolio/static/media/contentImage.de3018fe.jpg"
+  },
+  {
+    "revision": "e3c8aa0c6d4605a45dae25c8eb167c78",
+    "url": "/PersonalPortfolio/static/media/ermiAndAaron2.e3c8aa0c.png"
+  },
+  {
+    "revision": "5cfbe0b3bf30a82822fb183b823e9b87",
+    "url": "/PersonalPortfolio/static/media/food.5cfbe0b3.png"
+  },
+  {
+    "revision": "f6259bc590f4a42bda88801250cacd72",
+    "url": "/PersonalPortfolio/static/media/movies.f6259bc5.png"
+  },
+  {
+    "revision": "f8b0db7e5fb7f6ef28cfa081b7ae57ec",
+    "url": "/PersonalPortfolio/static/media/music.f8b0db7e.png"
+  },
+  {
+    "revision": "590edf13ca0fb63257bfabdb563e7c5e",
+    "url": "/PersonalPortfolio/static/media/myPhoto.590edf13.JPG"
+  },
+  {
+    "revision": "61f1049a749009729545ce2f119e1373",
+    "url": "/PersonalPortfolio/static/media/picsInToronto.61f1049a.JPG"
+  },
+  {
+    "revision": "4d6d788060b1401d78b7d19efd783cc7",
+    "url": "/PersonalPortfolio/static/media/running.4d6d7880.png"
+  },
+  {
+    "revision": "9ad6944f38600cb01e69eaf99a10c82e",
+    "url": "/PersonalPortfolio/static/media/simpson.9ad6944f.png"
+  },
+  {
+    "revision": "4aa375e5afeb84c521c6186272ed0c13",
+    "url": "/PersonalPortfolio/static/media/subBuzz.4aa375e5.png"
+  },
+  {
+    "revision": "ed251eb53b62119221d9cb648e2a7530",
+    "url": "/PersonalPortfolio/static/media/youngBlackMan.ed251eb5.png"
+  }
+]);
